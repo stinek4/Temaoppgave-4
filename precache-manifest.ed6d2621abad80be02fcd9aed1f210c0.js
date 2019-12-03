9167073bab27b7ca71899b4997c872c015968a0c
stinek4 <stinemvorner@gmail.com>
2019-12-03 15:05:58 +0100

    Updates

---
+++ precache-manifest.ed6d2621abad80be02fcd9aed1f210c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07390f3299f4d7c5a924d5b126cf271",
+    "url": "/Temaoppgave-4/index.html"
+  },
+  {
+    "revision": "558addfe1a7908cfca9b",
+    "url": "/Temaoppgave-4/static/css/main.942389eb.chunk.css"
+  },
+  {
+    "revision": "f89deffde9533b5a7ed8",
+    "url": "/Temaoppgave-4/static/js/2.a2be4329.chunk.js"
+  },
+  {
+    "revision": "558addfe1a7908cfca9b",
+    "url": "/Temaoppgave-4/static/js/main.6d58e25d.chunk.js"
+  },
+  {
+    "revision": "c14a3980f6b857e91372",
+    "url": "/Temaoppgave-4/static/js/runtime-main.ee023daf.js"
+  }
+]);
